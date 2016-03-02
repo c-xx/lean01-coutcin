22aa159becdd77153c03ffd4e7278c449aa69fec
edwin <sblig@126.om>
2016-03-02 23:07:27 +0800

    命名空间使用

--- coutcin/test.cpp
+++ coutcin/test.cpp
@@ -1,8 +1,41 @@
 #include <stdlib.h>
+#include <iostream>
+using namespace std;
 
+namespace A
+{
+	int x = 1;
+	void fun() {
+		cout << "A" << endl;
+	}
+}
+
+namespace B
+{
+	int x = 2;
+	void fun() {
+		cout << "B" << endl;
+	}
+
+	void fun2() {
+		cout << "2B" << endl;
+	}
+}
+
+using namespace B;
 int main(void)
 {
+	
+	cout << "hello" << endl;
+	
 
+	cout << A::x << endl;
+	B::fun();
+	fun2(); // 命名空间使用
+
+
+	system("pause");
+	return 0;
 }
 
 
